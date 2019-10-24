8a7ef42a319b6e1ff8be2685325781eb2197631e
Mikkel Lauritzen <mikkellauritzen@Mikkels-MacBook-Pro.local>
2019-10-24 15:31:07 +0100

    Addded display of past priorities, using localStorage

--- main.js
+++ main.js
@@ -1,20 +1,37 @@
+let priorityToday = ""; // today's priority!
+let listItems = ""; // string that we'll store our <li>s to display in
 
-var priorityToday; // today's priority!
+const pastPriorities = JSON.parse(localStorage.getItem('pastPriorities'));
 
-function savePriority() { // ensure that today's priority is saved in local storage, key = storedPriority
+function savePriority() { // ensure that today's priority is saved in local storage
    localStorage.setItem('storedPriority', priorityToday);
 }
 
-var priorityToday = localStorage.getItem('storedPriority'); // Ensures the default value is the stored value, e.g. for opening new tab
+function saveList() { // ensure that latest listItems string is saved in local storage
+   localStorage.setItem('pastPriorities', JSON.stringify(listItems));
+}
 
+priorityToday = localStorage.getItem('storedPriority'); // Ensures the default value is the stored value, e.g. for opening new tab
+
+// Ensure list of previous priorities gets displayed on page load if it's not empty:
+if(pastPriorities) {
+   listItems = pastPriorities;
+   }
 
 function getEncouragement() { // pushes the priority into the encouragement header "inner HTML"
    if (priorityToday) {
-      document.getElementById("encouragement").innerHTML  = `${priorityToday}!`;
+      document.getElementById("encouragement").innerHTML  = `${priorityToday}`;
    } else document.getElementById("encouragement").innerHTML  = ``;
 }
 
+function showList() { // pushes the overview of past priorities into the relevant <ul>
+   if (listItems) {
+      document.getElementById("overview-list").innerHTML  = `${listItems}`;
+   }
+}
+
 getEncouragement(); // run this whenever main.js loads, to ensure that the storedPriority is displayed e.g. on new tab
+showList(); // run this whenever main.js loads to ensure that list of previous priorities is displayed
 
 function changePriority() { // updates priorityToday with input value, saves to local storage, calls getEncouragement
 
@@ -41,18 +58,25 @@ document.getElementById("priority-form").addEventListener('submit', function(e) 
  });
 
 
- document.getElementById("reset-form").addEventListener('submit', function(e) {
+ document.getElementById("reset-form").addEventListener('submit', function(e) { // reset button
     
-   e.preventDefault(); // prevents page refresh
+    e.preventDefault(); // prevents page refresh
+
+    if (priorityToday) {
+      listItems += "<li>" + priorityToday + "</li>";
+      saveList();
+    } 
 
     priorityToday = ""; // sets priorityToday to empty string (which is falsy)
 
     savePriority(); // saves to local storage
+
+    showList(); // updates the inner HTML, showing the list of previous priorities
     
     document.getElementById("priority-form").style.display = 'block'; // display form
 
     document.getElementById("reset-form").style.display = 'none'; // hide reset button
 
-    getEncouragement(); // updates the inner HTML
+    getEncouragement(); // updates the inner HTML, showing the encouragement to do current top priority
 
  });
